e9758bf6b6de37ca1836e44c254718e0c0f467c1
Mart Velema <mart.velema@student.nhlstenden.com>
2025-05-02 01:26:28 +0200

    Added boilerplate for all the routes

---
+++ src/Friends.rs
@@ -0,0 +1,19 @@
+pub async fn show()
+{
+    
+}
+
+pub async fn index()
+{
+    
+}
+
+pub async fn store()
+{
+    
+}
+
+pub async fn destroy()
+{
+    
+}
---
+++ src/Game.rs
@@ -0,0 +1,24 @@
+pub async fn show()
+{
+    
+}
+
+pub async fn index()
+{
+    
+}
+
+pub async fn update()
+{
+    
+}
+
+pub async fn index_game_by_user()
+{
+    
+}
+
+pub async fn update_game_by_user()
+{
+    
+}
