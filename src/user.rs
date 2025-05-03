23c167d296f735bc857266e6403f15fd716cec51
Mart Velema <mart.velema@student.nhlstenden.com>
2025-05-03 17:19:24 +0200

    Added show functionality, implemented removal of response changes

--- src/user.rs
+++ src/user.rs
@@ -1,14 +1,13 @@
-use crate::response;
 use crate::database::CONN;
 
 use axum::Json;
-use axum::response::Html;
 use rusqlite::named_params;
 use serde::{Serialize, Deserialize};
+use serde::de::DeserializeOwned;
 use sql_query_builder as sql;
 
-#[derive(Serialize)]
-struct User
+#[derive(Deserialize, Serialize)]
+pub struct User
 {
     id: i32,
     username: String,
@@ -17,17 +16,44 @@ struct User
     points: i32
 }
 
-pub async fn index(body: String) -> Html<&'static str>
+pub async fn index(body: String) -> Result<Json<String>, Json<String>>
 {
-    Html("<h1>Hello, world!</h1>")
+    Ok(Json("Not implemented yet".to_string()))
 }
 
-pub async fn show()
+pub async fn show(body: String)  -> Result<Json<Vec<User>>, Json<String>>
 {
+    let query: String = sql::Select::new()
+        .select("*")
+        .from("users")
+        .as_string();
 
+    let conn = CONN.lock().unwrap();
+    let stmt = conn.prepare(&query);
+
+    let mut result = stmt.unwrap();
+
+    let users = result.query_map([], |row| {
+        Ok(User {
+            id: row.get::<usize, i32>(0).unwrap(),
+            username: row.get::<usize, String>(1).unwrap(),
+            email: row.get::<usize, String>(2).unwrap(),
+            password: row.get::<usize, String>(3).unwrap(),
+            points: row.get::<usize, i32>(4).unwrap()
+        })
+    }).unwrap();
+
+    let mut user_vector: Vec<User> = Vec::new();
+
+    for user in users
+    {
+        user_vector.push(user.unwrap());
+    };
+    
+    Ok(Json(user_vector))
 }
 
-pub async fn store(body: String) -> Result<Json<response::Response>, Json<response::Response>>
+pub async fn store(body: String) -> Result<Json<String>, Json<String>>
 {
     #[derive(Deserialize)]
     struct Request
@@ -36,15 +62,12 @@ pub async fn store(body: String) -> Result<Json<response::Response>, Json<respon
         email: String,
         password: String
     }
-    
-    let request: Request = match serde_json::from_str(&body)
-    {
+
+    let request: Request = match deserialize_request::<Request>(body.as_str()) {
         Ok(request) => request,
-        Err(error) => 
-            {
-                return Err(response::Response::new(&format!("Unable to create new user: {}", error))
-                    .to_json())
-            }
+        Err(error) => {
+            return Err(Json(format!("Unable to create new user: {}", error)));
+        }
     };
 
     let encrypted_password: String = bcrypt::hash(request.password, 12).unwrap();
@@ -69,8 +92,7 @@ pub async fn store(body: String) -> Result<Json<response::Response>, Json<respon
 
     if has_taken_credentials
     {
-        return Err(response::Response::new("This email or username is already in use")
-                .to_json())
+        return Err(Json("This email or username is already in use".to_string()))
     }
 
     let query: String = sql::Insert::new()
@@ -85,16 +107,23 @@ pub async fn store(body: String) -> Result<Json<response::Response>, Json<respon
         .unwrap()
         .execute(named_params! {":username": request.username, ":email": request.email, ":password": encrypted_password});
 
-    Ok(response::Response::new(&format!("Successfully created a new user: {}", request.username))
-           .to_json())
+    Ok(Json(format!("Successfully created a new user: {}", request.username)))
 }
 
-pub async fn update()
+pub async fn update() -> Result<Json<String>, Json<String>>
 {
-    
+    Ok(Json("Not implemented yet".to_string()))
 }
 
-pub async fn destroy()
+pub async fn destroy() -> Result<Json<String>, Json<String>>
 {
-    
+    Ok(Json("Not implemented yet".to_string()))
+}
+
+fn deserialize_request<T: DeserializeOwned>(body: &str) -> Result<T, String>
+{
+    match serde_json::from_str(body) {
+        Ok(request) => Ok(request),
+        Err(error) => Err(format!("Unable to create new user: {}", error)),
+    }
 }
